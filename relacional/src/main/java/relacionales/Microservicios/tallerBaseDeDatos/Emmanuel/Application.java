c5620ec2adc9f3a1687a42bb1ef350854f919ef4
Emmvnu3l <emm.moreno@duocuc.cl>
2025-12-14 23:37:49 -0300

    First Commit

---
+++ relacional/src/main/java/relacionales/Microservicios/tallerBaseDeDatos/Emmanuel/Application.java
@@ -0,0 +1,13 @@
+package relacionales.Microservicios.tallerBaseDeDatos.Emmanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
